34105dd114a3da6a59a29aae942cb0ac99490874
Paul Scarrone <pscarrone@thinkthroughmath.com>
2015-06-02 02:52:07 -0400

    reconstruct interactions and messages into something consumable since
    The api end point only allows restful references of 1 id deep so
    We have to nest and rely on promises to reconstruct the data and expand
    some of the objects for easy use

--- app/scripts/controllers/interaction.js
+++ app/scripts/controllers/interaction.js
@@ -25,7 +25,7 @@ angular.module('ngChatApp')
 
 angular.module('ngChatApp')
   .factory('Message', function($resource) {
-    return $resource('http://elymu-api.herokuapp.com/users/:uid/messages');
+    return $resource('http://elymu-api.herokuapp.com/threads/:tid/messages');
   });
 
 angular.module('ngChatApp')
@@ -35,32 +35,44 @@ angular.module('ngChatApp')
 
 angular.module('ngChatApp')
   .factory('Thread', function($resource) {
-    return $resource('http://elymu-api.herokuapp.com/messages/:mid/threads');
+    return $resource('http://elymu-api.herokuapp.com/interactions/:iid/threads');
   });
 
 angular.module('ngChatApp')
   .controller('InteractionCtrl', function ($scope, User, Thread, Interaction, Message) {
+    /* jshint ignore:start */
     User.query(function(data) {
-      $scope.users = data;
-      Message.query({uid: $scope.users[0].id}, function(data) {
-        $scope.messages = data;
-        console.log(data);
+      var uid = data[0].id;
+      $scope.users = function(data){
+        var users = [];
+        for(var i = 0; i<data.length; i++){
+          users[data[i].id] = data[i];
+        }
+        return users;
+      }(data);
+      $scope.user = data[0];
+      Interaction.query({uid: uid}, function(data) {
+        $scope.user.interactions = data;
+        for(var i=0; i<data.length; i++){
+          var iid = $scope.user.interactions[i].id;
+          $scope.user.interactions[i].recipient = $scope.users[$scope.user.interactions[i].recipientId];
+          var pos = i;
+          Thread.query({iid: iid}, function(data) {
+            $scope.user.interactions[pos].threads = data;
+            for(var j=0; j<data.length; j++){
+              var tpos = j;
+              var tid = $scope.user.interactions[pos].threads[j].id;
+              Message.query({tid: tid}, function(data) {
+                $scope.user.interactions[pos].threads[tpos].messages = data;
+                for(var g=0; g<data.length; g++){
+                  $scope.user.interactions[pos].threads[tpos].messages[g].recipient = $scope.users[$scope.user.interactions[pos].threads[tpos].messages[g].recipientId];
+                }
+              });
+            }
+          });
+        }
       });
     });
-
-
-    Thread.query(function(data) {
-      $scope.threads = data;
-    });
-
-    Interaction.query(function(data) {
-      $scope.interactions = data;
-    });
-
-    Message.query(function(data) {
-      $scope.messages = data;
-    });
-
-
+    /* jshint ignore:end */
     $scope.awesomeThings = [];
   });
